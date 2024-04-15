37db7b9efd8e6fea08bf8f87f9ea919ff140a44d
Muzhe Wu <635717823@qq.com>
2024-04-15 00:42:36 -0400

    update

--- src/components/Onboard.jsx
+++ src/components/Onboard.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import HeadBar from "./HeadBar";
 import AdaptiveTextarea from "./AdaptiveTextarea";
-import bizlingoBag from '../assets/bizlingo-bag.svg';
+import bizfly from '../assets/bizzie-fly.svg';
 import monologue from '../assets/monologue.svg';
 import dialogue from '../assets/dialogue.svg';
 
@@ -53,13 +53,13 @@ export default function Onboard({ stage, increStage, decreStage }) {
                         </div>
 
                         <div className="flex justify-center w-full my-10">
-                            <button className={`mr-10 flex flex-col items-center justify-center w-52 h-48 outline outline-2 ${interest === "Monologue"? "outline-4 outline-blue-200" : "outline-2 outline-gray-200"} rounded-lg`}
-                                onClick={()=> {setInterest("Monologue")}}>
+                            <button className={`mr-10 flex flex-col items-center justify-center w-52 h-48 outline outline-2 ${interest === "Monologue" ? "outline-4 outline-blue-200" : "outline-2 outline-gray-200"} rounded-lg`}
+                                onClick={() => { setInterest("Monologue") }}>
                                 <img src={monologue} width={100} className="mb-1" alt="Bizlingo Bag" />
                                 Monologue
                             </button>
-                            <button className={`flex flex-col items-center justify-center w-52 h-48 outline ${interest === "Dialogue"? "outline-4 outline-blue-200" : "outline-2 outline-gray-200"} rounded-lg`}
-                                onClick={()=> {setInterest("Dialogue")}}>
+                            <button className={`flex flex-col items-center justify-center w-52 h-48 outline ${interest === "Dialogue" ? "outline-4 outline-blue-200" : "outline-2 outline-gray-200"} rounded-lg`}
+                                onClick={() => { setInterest("Dialogue") }}>
                                 <img src={dialogue} width={100} className="mb-1" alt="Bizlingo Bag" />
                                 Dialogue
                             </button>
@@ -77,6 +77,30 @@ export default function Onboard({ stage, increStage, decreStage }) {
                                 CONTINUE
                             </button>
                         </div>
+                    </>
+                )
+            }
+            {
+                stage === 2 && (
+                    <>
+                        <svg width="800" height="176" viewBox="0 0 800 176" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M695.744 0.748199H-30.4512V175.735H708.243L695.744 0.748199Z" fill="#FFC800" />
+                            <rect x="41" y="24" width="511" height="26" rx="13" fill="#FFD300" />
+                            <defs>
+                                <radialGradient id="paint0_radial_111_948" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(750.697 34.4635) rotate(114.523) scale(140.271 181.97)">
+                                    <stop stop-color="#26FF55" />
+                                    <stop offset="0.466774" stop-color="#268AFF" />
+                                    <stop offset="0.973038" stop-color="#FC55FF" />
+                                </radialGradient>
+                                <radialGradient id="paint1_radial_111_948" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(750.697 34.4635) rotate(114.523) scale(140.271 181.97)">
+                                    <stop stop-color="#26FF55" />
+                                    <stop offset="0.466774" stop-color="#268AFF" />
+                                    <stop offset="0.973038" stop-color="#FC55FF" />
+                                </radialGradient>
+                            </defs>
+                        </svg>
+
+                        <img src={bizfly} width={200} className="fixed left-1/2 top-1/2 transform -translate-y-1/2 -translate-x-1/2" alt="Bizlingo Bag" />
                     </>
                 )
             }
